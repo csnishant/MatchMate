cec484413911c3b070fe1f1e779bb873815bfb98
Nishant Choudhary <cs.nishantchoudhary@gmail.com>
2025-12-15 23:32:45 +0530

    refactor: rename project to match-mate

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,3 +1,4 @@
+export const AUTH_API_END_POINT = "http://localhost:8000/api/auth";
 export const USER_API_END_POINT = "http://localhost:8000/api/user";
 export const Request_API_END_POINT = "http://localhost:8000/api/request";
 export const POST_API_END_POINT = "http://localhost:8000/api/user-post";
