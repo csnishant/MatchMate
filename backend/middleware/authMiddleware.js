5ce504c9c28109a34c79209bbfdf6deacf605a04
yashk-tech <yash155225@gmail.com>
2025-12-27 19:08:47 +0530

    validation testing

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,9 +1,9 @@
-import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken"; import abcd from "abcd";
 //correct
-const isAuthenticated = (req, res, next) => {
+const isAuthenticated = (req, res, next) => {   
   try {
-    const token = req.cookies.token;
-
+    const token = req.cookies.token;  
+             
     if (!token) {
       return res.status(401).json({
         message: "User not authenticated",
