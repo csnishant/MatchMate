4fdee53571f4169efc260402ae3a9ff1df053d41
Nishant Choudhary <cs.nishantchoudhary@gmail.com>
2026-04-19 23:01:02 +0530

    clean dependencies and downgrade express

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,3 +1,9 @@
+// export const AUTH_API_END_POINT = "http://localhost:8000/api/auth";
+// export const USER_API_END_POINT = "http://localhost:8000/api/user";
+// export const REQUEST_API_END_POINT = "http://localhost:8000/api/request";
+// export const POST_API_END_POINT = "http://localhost:8000/api/user-post";
+
+
 export const AUTH_API_END_POINT = "http://localhost:8000/api/auth";
 export const USER_API_END_POINT = "http://localhost:8000/api/user";
 export const REQUEST_API_END_POINT = "http://localhost:8000/api/request";
